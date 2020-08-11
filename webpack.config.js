037d27a96fa991c4d41302504c5e4935cf97d974
AndreyPanovA <panov3107@mail.ru>
2020-08-11 13:17:20 +0300

    html view

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require("path"); // просто для определения путей, можно и без него прописать нативно
 // __dirname -  путь до текущей папки
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // чистит папку dist, удаляет лишние файлы с hash
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: "development",
   entry: "./index.js",
   output: {
     //   Куда все складываем
-    filename: "bundle.js",
+    filename: "bundle.[hash].js", // добавляя hash мы получаем новую версию с hash названием, пока dist не чистится
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "index.html", // создает шаблон для html, чтобы самостоятельно его не генерировать (создавать)
+    }),
+  ],
 };
